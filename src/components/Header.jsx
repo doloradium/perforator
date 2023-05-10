7917e3536b7713cb74bfc94ced9e95c8db7890a4
Doloradium <valerymakkol@gmail.com>
2023-05-10 21:45:28 +0300

    Sync 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import styled from "styled-components"
 
 import logo from '../img/logo.svg'
@@ -39,47 +38,3 @@ function Header() {
 }
 
 export default Header;
-
-=======
-import styled from "styled-components"
-
-import logo from '../img/logo.svg'
-
-const Container = styled.div`
-    width: 30px; 
-    height: 20px;
-    display: flex;
-    flex-direction: column; 
-    justify-content: space-between;
-`
-
-const Line = styled.span`
-    width: 30px;
-    display: block;
-    height: 3px;
-    background-color: #fff;
-`
-
-const Wrapper = styled.section`
-    display: flex;
-    justify-content: space-between;
-    padding: 50px;
-    align-items: center;
-`
-
-function Header() {
-    return (
-        <Wrapper>
-            <img src={logo} alt="AO Performance" />
-            <Container>
-                <Line></Line>
-                <Line></Line>
-                <Line></Line>
-            </Container>
-        </Wrapper>
-    )
-}
-
-export default Header;
-
->>>>>>> Stashed changes
